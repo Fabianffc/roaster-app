58b19dfa9870cb5bb7bf1f128c3003ac4b310244
Fabian <fabianffc@gmail.com>
2021-02-06 14:06:38 -0600

    greeting display comparing to our date

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,22 @@
-function Welcome(){
-    return <h2 >Welcome to Astros Fans!!⚾️</h2>
+import { playerData } from "../data/playerData"
+
+function Welcome() {
+    const currentHour = new Date().getHours()
+    const message = currentHour < 5 ? "Why are you still up?" 
+                    : currentHour < 12 ? "Good Morning"
+                    : currentHour < 18 ? "Good Afternoon"
+                    :  "Good Evening"
+//You can also do it this way 
+/*
+
+*/
+
+    return (
+        <>
+            <p>{message} sports fans!</p>
+            <h2 >Welcome to Astros Fans!!⚾️</h2>
+        </>
+    )
 }
 
 export default Welcome;
